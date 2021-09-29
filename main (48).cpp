292e7c71f7f212e6c12c1bc103b09f8dd66a57d3
Ragini kumari <88733937+Raginikumari15@users.noreply.github.com>
2021-09-30 02:36:54 +0530

    Add files via upload

---
+++ main (48).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{                          // finding largest num in arry
+    int   n = 4;
+int A[4]={88,768,2,3};
+int max = A[0];
+max = A[0];
+for(int i = 1; i<n; i++){
+  if(A[i]> max){
+      max = A[i];
+      
+  } 
+}
+  cout<< max;
+}
